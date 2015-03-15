0e7c1de08f4e869afe786c472d6f9aeedf327202
Matteo46 <matteo.4627@yahoo.it>
2015-03-15 19:22:59 +0100

    Contatore di parole di un romanzo da un file di testo

---
+++ Romanzo/RomanzoSet/src/romanzo/Romanzo.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package romanzo;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Studente
+ */
+public class Romanzo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+     String inputFile = "dati/romanzo.txt";
+TreeSet d = new TreeSet();
+     
+int dimTesto = contaParole(inputFile);
+              String testo[] = new String[dimTesto];	
+			d.add(dimTesto);
+              
+System.out.println("come poter lavorare col testo:\n 1) Conta numero di parole\n 2)conta numero di parole diverse\n 3) esci");
+         int scelta = Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
+
+		
+			switch (scelta) {
+			
+				                
+			case 1: {
+				System.out.println("Il numero di parole nel testo e' " + d);
+			};
+			
+			
+			break;
+			case 2: 
+                        {
+		         System.out.println("Il numero di parole diverse è " + contaparolediverse(inputFile));	
+			};
+			break;
+                        case 3:{
+                            System.out.println("Uscita dal programma...");
+                        };
+                       
+			}
+			
+			
+		}
+	
+	
+
+
+	// Conta il numero di parole contenute nel testo
+public static int contaParole(String inputFile) throws IOException {
+		int conto = 0;
+		
+                StringTokenizer riga = null;
+		BufferedReader in = new BufferedReader(new FileReader(inputFile));
+		String s = in.readLine();
+		while(s != null) {
+			riga = new StringTokenizer(s, " \t\n\r\f,.;:");
+			conto = conto + riga.countTokens();
+			s = in.readLine();
+		}
+		return conto;
+	          
+            
+
+}    	 
+public static int contaparolediverse(String inputFile) throws IOException{
+    int conto =0;   
+    int dimTesto = contaParole(inputFile);
+String testo[] = new String [dimTesto];
+    for (int i = 0; i < testo.length; i++) {
+        for (int j = testo.length-1; j <=1; j--) {
+          if(testo[i].equalsIgnoreCase(testo[j])){
+              testo[i] = testo[j];
+          conto ++;            
+          while(testo[i].contentEquals(testo[j])){
+              conto = conto+2;
+          }
+          }
+    
+        }
+}
+return conto;
+}
+}
+
+		        
+
+
+    
+
+
+	
